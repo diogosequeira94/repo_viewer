706e84ac896d8d3ff6ee4bbbc8a58b7cefa841c3
Diogo Sequeira <diogo.sequeira@ctw.bmwgroup.com>
2024-07-20 15:58:56 +0100

    add dependencies for project

---
+++ lib/auth/domain/entity.dart
@@ -0,0 +1,7 @@
+/// This layer does not contain any Flutter code.
+/// Plain dart objects
+///
+
+/// Failures and Entities will be put into an Either Type as they travel from the repsitories
+/// When we get to the bloc we de-construct this Either Type and return the states accordingly
+class PlaceholderEntity {}
---
+++ lib/auth/repository/dto/dto.dart
@@ -0,0 +1,5 @@
+/// Imitates the entity but can get a bit messier
+/// Can have dependencies
+/// That are not pure entities
+/// We convert DTOs to Entities
+class DtoPlaceHolder {}
